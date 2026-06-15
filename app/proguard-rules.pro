14cb3c262d1fe3824f208d2d0877a962901231b2
androhimanshu <himanshu.mainid@gmail.com>
2026-06-15 15:51:18 +0530

    Delete app directory

--- app/proguard-rules.pro
+++
@@ -1,2 +0,0 @@
-# Keep model classes that use reflection-free JSON (no rules needed)
--keepattributes SourceFile,LineNumberTable
--- app/src/main/java/com/touchtrace/app/TouchTraceApp.java
+++
@@ -1,6 +0,0 @@
-package com.touchtrace.app;
-
-import android.app.Application;
-
-/** Application entry point. */
-public final class TouchTraceApp extends Application { }
